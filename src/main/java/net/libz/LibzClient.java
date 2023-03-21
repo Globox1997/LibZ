28954d541347db3bd005dc5102958744b4dcca74
Globox1997 <mitschke11@web.de>
2023-03-21 20:46:13 +0100

    moved to registry

--- src/main/java/net/libz/LibzClient.java
+++ src/main/java/net/libz/LibzClient.java
@@ -7,9 +7,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.libz.api.InventoryTab;
-import net.libz.util.SortList;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
@@ -21,25 +18,6 @@ public class LibzClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // TEST
-        // registerInventoryTab(new InventoryTab(InventoryScreen.class, Text.of("TEST"), new Identifier("libz:textures/gui/test.png"), new Identifier("test:test"), 0));
-        // registerInventoryTab(new InventoryTab(InventoryScreen.class, Text.of("TEST3"), new Identifier("libz:textures/gui/test.png"), new Identifier("test:test"), 7));
-        // registerInventoryTab(new InventoryTab(InventoryScreen.class, Text.of("TEST2"), new Identifier("libz:textures/gui/test.png"), new Identifier("test:test"), 3));
-        // registerInventoryTab(new InventoryTab(InventoryScreen.class, Text.of("TEST1"), new Identifier("libz:textures/gui/test.png"), new Identifier("test:test"), 1));
-    }
-
-    public static void registerInventoryTab(InventoryTab tab) {
-        LibzClient.inventoryTabs.add(tab);
-        // Sort prefered pos
-        List<Integer> priorityList = new ArrayList<Integer>();
-        for (int i = 0; i < inventoryTabs.size(); i++) {
-            int preferedPos = inventoryTabs.get(i).getPreferedPos();
-            if (preferedPos == -1) {
-                preferedPos = 99;
-            }
-            priorityList.add(preferedPos);
-        }
-        SortList.concurrentSort(priorityList, inventoryTabs);
     }
 
 }
---
+++ src/main/java/net/libz/registry/TabRegistry.java
@@ -0,0 +1,25 @@
+package net.libz.registry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.libz.LibzClient;
+import net.libz.api.InventoryTab;
+import net.libz.util.SortList;
+
+public class TabRegistry {
+
+    public static void registerInventoryTab(InventoryTab tab) {
+        LibzClient.inventoryTabs.add(tab);
+        // Sort prefered pos
+        List<Integer> priorityList = new ArrayList<Integer>();
+        for (int i = 0; i < LibzClient.inventoryTabs.size(); i++) {
+            int preferedPos = LibzClient.inventoryTabs.get(i).getPreferedPos();
+            if (preferedPos == -1) {
+                preferedPos = 99;
+            }
+            priorityList.add(preferedPos);
+        }
+        SortList.concurrentSort(priorityList, LibzClient.inventoryTabs);
+    }
+}
